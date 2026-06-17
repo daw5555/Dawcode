b63201892a91f2b8c7e12a19211c39f4079d2a63
daw5555 <paparwadee300851@gmail.com>
2026-06-17 03:15:38 +0000

    program01.ty

---
+++ program01.py
@@ -0,0 +1,6 @@
+print("โปรเเกรมคำนวณหาพื้นที่สามเหลี่ยม")
+print("--------------------------")
+tall = float(input("enter hieght:"))
+base = float(input("entr base:"))
+areatri = 0.5*tall*base
+print("พื้นที่สามเหลียม=",areatri)
